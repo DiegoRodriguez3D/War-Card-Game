bab243b2f17683c626bce7db49bb3f0cbeea1588
DiegoRodriguez3D <69866476+DiegoRodriguez3D@users.noreply.github.com>
2024-08-26 10:00:10 +0200

    Initial Commit

---
+++ War Card Game/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  War Card Game
+//
+//  Created by Diego Rodriguez on 26/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
